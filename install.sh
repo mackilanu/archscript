8187347852621dce12cfed292d43708ccd6bfcba
Marcus Andersson <mackilanu@gmail.com>
2021-10-20 16:25:53 +0200

    Adding reflector

--- install.sh
+++ install.sh
@@ -9,21 +9,34 @@ makepkg -si
 cd ~
 rm -rf yay/
 
+#Creating standard dirs
+mkdir Pictures
+mkdir Videos
+mkdir Music
+mkdir Dicuments
+mkdir Templates
+
 #Install vim and it's configuration
 echo Installing vim config...
 sudo pacman -S vim git
+mkdir -vp ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/mackilanu/dot.git
 mv ~/dot/.vimrc ~/.vimrc
 
 #Install i3 and configuring it...
 echo Installing i3 and configuring...
-sudo pacman -S i3-gaps i3status i3lock i3status rofi picom network-manager-applet blueman blueman feh xorg xorg-xinit dunst firefox linux-lts
+sudo pacman -S i3-gaps i3status i3lock i3status rofi picom network-manager-applet blueman blueman feh xorg xorg-xinit dunst firefox linux-lts reflector
 mkdir -pv ~/.config/i3
 mv ~/dot/i3_config ~/.config/i3/config
 sudo mv ~/dot/picom_config /etc/xdg/picom.conf
 mv ~/dot/bg.jpg ~/Pictures
 mv ~/dot/.xinitrc ~
+
+#Configuring reflector
+echo Configuring reflector...
+mkdir -p /etc/xdg/reflector
+mv ~/dot/reflector.conf /etc/xdg/reflector
 
 #Configure terminator
 echo Installing terminator and configuring it
@@ -34,6 +47,7 @@ mv ~/dot/terminator_config ~/config/terminator/config
 echo Enabling some systemd stuff..
 sudo systemctl enable NetworkManager
 sudo systemctl enable bluetooth
+sudo systemctl enable reflector --now
 
 #Configure grub
 echo Configuring grub...
