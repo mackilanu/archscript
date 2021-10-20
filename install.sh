35ee02af2500b376efeebfa74bf2251be9bb9281
Marcus Andersson <mackilanu@gmail.com>
2021-10-20 13:41:31 +0200

    Adding grub confog

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #Install yay
 echo Installing yay...
 sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git ~
+git clone https://aur.archlinux.org/yay.git
 cd ~/yay
 makepkg -si
 cd ~
@@ -12,13 +12,13 @@ rm -rf yay/
 #Install vim and it's configuration
 echo Installing vim config...
 sudo pacman -S vim git
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ~
-git clone https://github.com/mackilanu/dot.git ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/mackilanu/dot.git
 mv ~/dot/.vimrc ~/.vimrc
 
 #Install i3 and configuring it...
 echo Installing i3 and configuring...
-sudo pacman -S i3-gaps i3status i3lock i3status rofi picom network-manager-applet blueman-applet blueman feh xorg-xinit dunst firefox
+sudo pacman -S i3-gaps i3status i3lock i3status rofi picom network-manager-applet blueman-applet blueman feh xorg-xinit dunst firefox linux-lts
 mkdir -v ~/.config/i3
 mv ~/dot/i3_config ~/.config/i3/config
 sudo mv ~/dot/picom_config /etc/xdg/picom.conf
@@ -31,9 +31,15 @@ sudo pacman -S terminator
 mkdir ~/.config/terminator
 mv ~/dot/terminator_config ~/config/terminator/config
 
-
 echo Enabling some systemd stuff..
 sudo systemctl enable NetworkManager
 sudo systemctl enable bluetooth
+
+#Configure grub
+echo Configuring grub...
+sudo mv ~/dot/grub /etc/default
+tar -xf ~/dot/Vimix.tar
+cd Vimix-1080p/
+sudo ./install.sh
 
 echo All done! Restart, login and run startx!
